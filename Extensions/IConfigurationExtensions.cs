fa679b0fd275fa92dd914b945652f717b3104d24
Steve Lamotte <steve@lexor.ca>
2018-01-24 15:41:16 -0600

    Add support to SqlServerSpatial to create spatial indexes

--- Extensions/IConfigurationExtensions.cs
+++ Extensions/IConfigurationExtensions.cs
@@ -7,10 +7,10 @@ namespace Lexor.GeoMedia.Extensions
     // ReSharper disable once InconsistentNaming
     public static class IConfigurationExtensions
     {
-        public static void RegisterGeoMediaServices(this IConfiguration configuration, IServiceCollection services, string geoMediaSettingSectionName)
+        public static void RegisterGeoMediaServices(this IConfiguration configuration, IServiceCollection services, string geoMediaSettingsSectionName)
         {
             services.AddSingleton<GeoMediaMetadata, GeoMediaMetadata>();
-            services.Configure<GeoMediaSettings>(configuration.GetSection(geoMediaSettingSectionName));
+            services.Configure<GeoMediaSettings>(configuration.GetSection(geoMediaSettingsSectionName));
         }
     }
 }
--- GeoMediaDbContext.cs
+++ GeoMediaDbContext.cs
@@ -1,17 +1,21 @@
 ﻿using Lexor.Data.SqlServerSpatial;
+using Lexor.Data.SqlServerSpatial.Settings;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 namespace Lexor.GeoMedia
 {
     public class GeoMediaDbContext : SqlServerSpatialDbContext
     {
-        public GeoMediaDbContext(DbContextOptions options)
-            : base(options) { }
+        protected GeoMediaDbContext(DbContextOptions options, IOptions<SqlServerSpatialSettings> settings)
+            : base(options, settings) { }
 
-        protected override void PerformAdditionalSpatialColumnProcessing(ModelBuilder modelBuilder, IEntityType entityType, PropertyInfo property)
+        protected override void PerformAdditionalSpatialColumnProcessing(ModelBuilder modelBuilder, IEntityType entityType, PropertyInfo property, SqlServerSpatialEntityMetadata metadata)
         {
+            base.PerformAdditionalSpatialColumnProcessing(modelBuilder, entityType, property, metadata);
+
             // Add requried GeoMedia version of geometry as a shadow field. This will be stored in the database but invisible to the model.
             // This will be the "primary" geometry field, and the previous field will be the "native" geometry field.
             modelBuilder
