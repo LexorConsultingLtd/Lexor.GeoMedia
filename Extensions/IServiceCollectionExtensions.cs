0401f41cdaa66b9279a3baea54619076291425f1
Steve Lamotte <steve@lexor.ca>
2018-04-05 11:10:12 -0700

    Store data protection keys (i.e. for Remember Me etc) in Azure blobs

--- Extensions/IServiceCollectionExtensions.cs
+++ Extensions/IServiceCollectionExtensions.cs
@@ -5,9 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Lexor.GeoMedia.Extensions
 {
     // ReSharper disable once InconsistentNaming
-    public static class IConfigurationExtensions
+    public static class IServiceCollectionExtensions
     {
-        public static void RegisterGeoMediaServices(this IConfiguration configuration, IServiceCollection services, string geoMediaSettingsSectionName)
+        public static void RegisterGeoMediaServices(this IServiceCollection services, IConfiguration configuration, string geoMediaSettingsSectionName)
         {
             services.AddSingleton<GeoMediaMetadata, GeoMediaMetadata>();
             services.Configure<GeoMediaSettings>(configuration.GetSection(geoMediaSettingsSectionName));
